12f96b8e2c6a97e6684191bb49152fedbba4b10a
Limbert Lino <limbertlino@gmail.com>
2024-10-14 08:27:53 -0400

    WIP

---
+++ exercises/05_vecs/vecs1.rs
@@ -0,0 +1,26 @@
+fn array_and_vec() -> ([i32; 4], Vec<i32>) {
+    let a = [10, 20, 30, 40]; // Array
+
+    let mut v = vec![];
+
+    for number in a.iter() {
+        v.push(*number)
+    }
+
+    (a, v)
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_array_and_vec_similarity() {
+        let (a, v) = array_and_vec();
+        assert_eq!(a, *v);
+    }
+}
